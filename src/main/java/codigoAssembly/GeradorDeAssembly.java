d5a41de9898f5956c0ec2e8b8910863e7e09c538
SAnjos <guilherme.sanjos@gmail.com>
2011-11-09 08:29:37 -0200

    Traducao do label e Call em assembly

--- src/main/java/codigoAssembly/GeradorDeAssembly.java
+++ src/main/java/codigoAssembly/GeradorDeAssembly.java
@@ -9,6 +9,77 @@ import codigoIntermediario.ConstrucaoDeQuatroEnderecos;
 
 public class GeradorDeAssembly {
 	
+	private List<ConstrucaoDeQuatroEnderecos> codigoIntermediario;
+	private int contadorDeInstrucao;
+	private BlocoAssembly blocoAtual;
+	
+	
+	private GeradorDeAssembly() {
+		this.carregaInstrucoes();
+		this.reseta();
+	}
+	
+	private boolean nomeDoOperadorAtualEh(String nome) {
+		if (this.codigoIntermediario != null && this.codigoIntermediario.get(contadorDeInstrucao) != null)
+			return this.codigoIntermediario.get(contadorDeInstrucao).getOperador().equalsIgnoreCase("label");
+		
+		return false;
+	}
+	
+	public void carregaInstrucoes() {
+		this.codigoIntermediario = CodigoIntermediario.getCodigo();
+		this.reseta();
+	}
+	
+	private ConstrucaoDeQuatroEnderecos instrucaoAtual() {
+		if (this.codigoIntermediario.size() > this.contadorDeInstrucao)
+			return null;
+		
+		ConstrucaoDeQuatroEnderecos proximo;
+		proximo = this.codigoIntermediario.get(contadorDeInstrucao);
+		return proximo;
+	}
+	
+	private ConstrucaoDeQuatroEnderecos proximaInstrucao() {
+		ConstrucaoDeQuatroEnderecos proximo = this.instrucaoAtual();
+		this.contadorDeInstrucao ++;
+		return proximo;
+	}
+	
+	private void reseta() {
+		this.contadorDeInstrucao = 0;
+		this.codigoIntermediario = null;
+		this.blocoAtual = null;
+	}
+	
+	private void traduzLabel() {
+		String nomeDoLabel = this.instrucaoAtual().getElementoAEsquerda();
+		blocoAtual = CodigoAssembly.getInstancia().getBloco(nomeDoLabel);
+	}
+	
+	private void traduzCall() {
+		String nomeMetodo = instrucaoAtual().getElementoAEsquerda();
+		int quantidadeParametros = Integer.parseInt(instrucaoAtual().getElementoADireita());
+		blocoAtual.addInstrucao("call", nomeMetodo, "");
+	}
+	
+	public void traduzir() {
+		CodigoAssembly.limpar();
+		
+		while (instrucaoAtual() != null) {
+			
+			if (nomeDoOperadorAtualEh("label"))
+				this.traduzLabel();
+			
+			if (nomeDoOperadorAtualEh("call"))
+				this.traduzCall();
+				
+				
+			
+			this.proximaInstrucao();
+		}
+			
+	}
 	
 	public void gerarBlocoPrincipal() {
 		BlocoAssembly main =  CodigoAssembly.getInstancia().getBloco("_main");
